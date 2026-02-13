b513c4ea1dd692a2e1909f787bbcee4f03a089c2
Daniil Razdymakha <daniilrazdymaha@gmail.com>
2026-02-13 13:24:25 +0700

    feat: add work with statistics

--- src/main/java/edu/common/handler/Handler.java
+++ src/main/java/edu/common/handler/Handler.java
@@ -1,10 +1,11 @@
 package edu.common.handler;
 
+import edu.common.statistics.StatisticsType;
 import edu.common.type.DataType;
 
 import java.util.List;
 import java.util.Map;
 
 public interface Handler {
-    Map<DataType, List<String>> processFileContents();
+    Map<DataType, List<String>> processFiles(StatisticsType statsType);
 }
--- src/main/java/edu/common/handler/HandlerFile.java
+++ src/main/java/edu/common/handler/HandlerFile.java
@@ -2,6 +2,9 @@ package edu.common.handler;
 
 import edu.common.exception.ProcessingException;
 import edu.common.filter.DataTypeFilter;
+import edu.common.statistics.Statistics;
+import edu.common.statistics.StatisticsFactory;
+import edu.common.statistics.StatisticsType;
 import edu.common.type.DataType;
 import edu.common.validator.DataTypeValidator;
 import edu.common.view.View;
@@ -15,74 +18,70 @@ import java.util.*;
 
 public class HandlerFile implements Handler {
 
-  private static final String FILE_NOT_EXIST_MESSAGE = "Файл '%s' не существует. Удалено.";
-  private static final String EXISTING_FILES_MESSAGE = "Существующие файлы: ";
+    private static final String FILE_NOT_EXIST_MESSAGE = "Файл '%s' не существует.";
 
-  private final List<Path> filePaths;
-  private final View view;
-  private final DataTypeFilter dataFilter;
+    private final List<Path> filePaths;
+    private final View view;
+    private final DataTypeFilter dataFilter;
 
-  public HandlerFile(List<Path> filePaths, View view, DataTypeValidator validator) {
-    this.filePaths = filePaths;
-    this.view = view;
-    this.dataFilter = new DataTypeFilter(validator);
-  }
-
-  @Override
-  public Map<DataType, List<String>> processFileContents() {
-
-    removeNonExistentFiles();
-
-    if (filePaths.isEmpty()) {
-      view.printMessage("Нет существующих файлов для обработки.");
-      return Collections.emptyMap();
+    public HandlerFile(List<Path> filePaths, View view, DataTypeValidator validator) {
+        this.filePaths = filePaths;
+        this.view = view;
+        this.dataFilter = new DataTypeFilter(validator);
     }
 
-    view.printMessage(EXISTING_FILES_MESSAGE + filePaths);
+    @Override
+    public Map<DataType, List<String>> processFiles(StatisticsType statsType) {
 
-    try {
+        removeNonExistentFiles();
 
-      Map<DataType, List<String>> filteredData = readAndFilterFiles();
-      printFilteredData(filteredData);
-
-      return filteredData;
-
-    } catch (IOException e) {
-      throw new ProcessingException("Ошибка обработки файлов", e);
-    }
-  }
-
-  private Map<DataType, List<String>> readAndFilterFiles() throws IOException {
-    for (Path filePath : filePaths) {
-      readFile(filePath);
-    }
-
-    return dataFilter.getFilteredData();
-  }
-
-  private void readFile(Path filePath) throws IOException {
-    try (BufferedReader bf = Files.newBufferedReader(filePath, StandardCharsets.UTF_8)) {
-      String line;
-      while ((line = bf.readLine()) != null) {
-        if (!line.isBlank()) {
-          dataFilter.processLines(line);
+        if (filePaths.isEmpty()) {
+            view.printMessage("Повторите ввод существующих файлов!");
+            return Collections.emptyMap();
         }
-      }
+
+        try {
+            readAndFilterFiles();
+            Map<DataType, List<String>> filteredData = dataFilter.getFilteredData();
+
+            if (statsType != null) {
+                Statistics statistics = StatisticsFactory.create(statsType);
+                String stats = statistics.getStatistics(filteredData);
+                view.printMessage(stats);
+            }
+
+            return filteredData;
+
+        } catch (IOException e) {
+            throw new ProcessingException("Ошибка обработки файлов", e);
+        }
     }
-  }
 
-  private void removeNonExistentFiles() {
-    filePaths.removeIf(
-        path -> {
-          if (!Files.exists(path)) {
-            view.printMessage(FILE_NOT_EXIST_MESSAGE + path);
-            return true;
-          }
-          return false;
-        });
-  }
+    private void readAndFilterFiles() throws IOException {
+        for (Path filePath : filePaths) {
+            readFile(filePath);
+        }
+    }
 
-  private void printFilteredData(Map<DataType, List<String>> data) {
-    data.forEach((type, values) -> view.printMessage(type.name() + " : " + values));
-  }
+    private void readFile(Path filePath) throws IOException {
+        try (BufferedReader bf = Files.newBufferedReader(filePath, StandardCharsets.UTF_8)) {
+            String line;
+            while ((line = bf.readLine()) != null) {
+                if (!line.isBlank()) {
+                    dataFilter.filter(line);
+                }
+            }
+        }
+    }
+
+    private void removeNonExistentFiles() {
+        filePaths.removeIf(
+                path -> {
+                    if (!Files.exists(path)) {
+                        view.printfMessage(FILE_NOT_EXIST_MESSAGE, path);
+                        return true;
+                    }
+                    return false;
+                });
+    }
 }
